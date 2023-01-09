5558ca7416ba9077339636f5b0e4d577253ec885
JakFranceschini <jakson.franceschini@gmail.com>
2023-01-08 21:07:27 -0300

    melhorias em geral

---
+++ script.js
@@ -0,0 +1,8 @@
+let barraBusca = document.getElementById('barraBusca')
+let iconeLupa = document.getElementById('iconeLupa')
+let iconeFacebook = document.getElementById('iconeFacebook')
+
+barraBusca.addEventListener('click', function() {
+    iconeLupa.style.display = 'none'
+    iconeFacebook.style.display = 'none'
+})
